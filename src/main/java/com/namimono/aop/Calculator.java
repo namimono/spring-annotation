170021d3dae63364ce9542a23a467fc78cef43ab
kami <42517157+kami@users.noreply.github.com>
2019-05-30 11:11:18 +0800

    Finished add aop

---
+++ src/main/java/com/namimono/aop/Calculator.java
@@ -0,0 +1,9 @@
+package com.namimono.aop;
+
+public class Calculator {
+
+    public Integer add(int a,int b){
+        System.out.println("adding..........");
+        return a+b;
+    }
+}
--- src/main/java/com/namimono/config/MyBeanPostProcessor.java
+++ src/main/java/com/namimono/config/MyBeanPostProcessor.java
@@ -6,13 +6,11 @@ import org.springframework.stereotype.Component;
 //如果BeanPostProcessor被加入到IOC容器中，所有Bean初始化时都会用到
 @Component
 public class MyBeanPostProcessor implements BeanPostProcessor {
-    @Override
     public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
         System.out.println(s+":postProcessBeforeInitialization");
         return o;
     }
 
-    @Override
     public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
         System.out.println(s+":postProcessAfterInitialization");
         return o;
